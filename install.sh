1194977de5387c84a327a5741ac0b5e4c94343da
busti <oss@busti.cool>
2021-06-24 19:36:55 +0200

    cd into install dir

--- install.sh
+++ install.sh
@@ -3,10 +3,12 @@
 
 script_dir=$(dirname $(readlink --canonicalize-existing $0))
 
-mkdir --parents $HOME/.config/nixpkgs
-rm -rf $HOME/.config/nixpkgs/*
-ln -sdf $script_dir/nixpkgs/* $HOME/.config/nixpkgs/
+( cd $script_dir
+  mkdir --parents $HOME/.config/nixpkgs
+  rm -rf $HOME/.config/nixpkgs/*
+  ln -sdf $script_dir/nixpkgs/* $HOME/.config/nixpkgs/
 
-nix-shell -E \
-  "with import <nixpkgs> {}; mkShell { buildInputs = [ (callPackage ./nixpkgs/packages/home-manager.nix {}) ]; }" \
-  --run "home-manager switch"
+  nix-shell -E \
+    "with import <nixpkgs> {}; mkShell { buildInputs = [ (callPackage ./nixpkgs/packages/home-manager.nix {}) ]; }" \
+    --run "home-manager switch"
+)
