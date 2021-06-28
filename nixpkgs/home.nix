b99349c54131aad198663b07a652c7f663d71162
busti <oss@busti.cool>
2021-06-29 00:15:50 +0200

    install nvtop

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -1,11 +1,15 @@
 {config, pkgs, ...}: let
+  optimus_java_launcher = pkgs.writeShellScriptBin "minecraft_java_launcher" ''
+    exec -a "$0" optirun /nix/store/p21v8k2jzmkvr2zlqpv1y11jrdsfm43w-openjdk-16+36/bin/java "$@"
+  '';
+
   system = (import <nixpkgs/nixos> {}).config;
 in {
   nixpkgs.config.allowUnfree = true;
 
   home.packages = with pkgs; [
     coreutils
-    # noncoreutils
+    # utils
     ripgrep nmap
     # applications
     jetbrains.idea-community jetbrains.webstorm jetbrains.clion
