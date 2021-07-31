1aefdaa4d4479933844fc7168a631763be9afe65
busti <oss@busti.cool>
2021-07-31 21:10:51 +0200

    create custom intellij install

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -3,6 +3,35 @@
     exec -a "$0" optirun /nix/store/p21v8k2jzmkvr2zlqpv1y11jrdsfm43w-openjdk-16+36/bin/java "$@"
   '';
 
+  primecraft = pkgs.minecraft.override rec {
+    jre = pkgs.jre.overrideAttrs (old@{installPhase ? "", ...}: {
+      nativeBuildInputs = old.nativeBuildInputs ++ [ pkgs.makeWrapper pkgs.primus ];
+      installPhase = installPhase + ''
+        mv $out/bin/java $out/bin/.java-wrapped
+        echo "#! ${pkgs.bash}/bin/bash" >> $out/bin/java
+        echo "script_dir=\$(dirname \$(readlink --canonicalize-existing \$0))" >> $out/bin/java
+        echo "export vblank_mode=0" >> $out/bin/java
+        echo 'primusrun $script_dir/.java-wrapped "$@"' >> $out/bin/java
+        chmod +x $out/bin/java
+      '';
+    });
+  };
+
+  insmellyj = with pkgs; jetbrains.idea-community.overrideAttrs (old@{fixupPhase ? "", ...}: {
+    fixupPhase = fixupPhase + ''
+      (
+        wrapProgram $out/bin/idea-community \
+          --prefix PATH : "${scala}/bin" \
+          --prefix PATH : "${sbt}/bin" \
+          --prefix PATH : "${erlang}/bin" \
+          --prefix PATH : "${elixir}/bin" \
+          --prefix PATH : "${nodejs}/bin" \
+          --prefix PATH : "${docker}/bin" \
+          --prefix PATH : "${docker-compose}/bin"
+      )
+    '';
+  });
+
   system = (import <nixpkgs/nixos> {}).config;
 in {
   nixpkgs.config.allowUnfree = true;
@@ -12,12 +41,12 @@ in {
     # utils
     ripgrep nmap
     # applications
-    jetbrains.idea-community jetbrains.webstorm jetbrains.clion
+    insmellyj jetbrains.webstorm jetbrains.clion
     openscad prusa-slicer freecad kicad
     dfeet qdirstat remmina
     spotify
     # games
-    minecraft superTuxKart
+    steam primecraft superTuxKart
   ];
 
   programs = {
